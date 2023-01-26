2da1119863d66c979475473dc214b107121b811b
pockerman <a.giavaras@gmail.com>
2023-01-26 08:20:25 +0000

    Add setup scripts

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='navalmartin_mir_aws_utils',
+    packages=['navalmartin_mir_aws_utils'],
+    version='0.0.3',
+    description='Various utilities for working with AWS in Python 3.',
+    author='Alexandros Giavaras',
+    author_email='a.giavaras@gmail.com',
+    url='https://github.com/pockerman/awsutils',
+    download_url='https://github.com/pockerman/awsutils/awsutils-0.0.3.tar.gz',
+    keywords=['aws', 'aws-utilities'],
+    classifiers=[],
+    scripts=[],
+)
