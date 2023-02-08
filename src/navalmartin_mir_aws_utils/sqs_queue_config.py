ffe57202dacb79a6f7ac51dba56d550279bd1da1
pockerman <a.giavaras@gmail.com>
2023-02-08 16:08:06 +0000

    Add new attribute for sqs configuration

--- src/navalmartin_mir_aws_utils/sqs_queue_config.py
+++ src/navalmartin_mir_aws_utils/sqs_queue_config.py
@@ -14,8 +14,10 @@ class SQSMessageConfig(object):
     def __init__(self, message_body: str,
                  message_group_id: str,
                  message_attributes: Union[MessageAttributes, None],
+                 message_deduplication_id: str,
                  delay_seconds: int = 123):
         self.message_body: str = message_body
         self.message_group_id = message_group_id
+        self.message_deduplication_id = message_deduplication_id
         self.message_attributes = message_attributes
         self.delay_seconds = delay_seconds
