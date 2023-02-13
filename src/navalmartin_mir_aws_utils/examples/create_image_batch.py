f25ff37d1948e7a6ab6820138a1f3336b718fad5
pockerman <a.giavaras@gmail.com>
2023-02-13 11:45:52 +0000

    Fix exposed credentials

--- src/navalmartin_mir_aws_utils/examples/create_image_batch.py
+++ src/navalmartin_mir_aws_utils/examples/create_image_batch.py
@@ -5,7 +5,13 @@ AWS_REGION = "YOUR_AWS_REGION"
 AWS_S3_BUCKET_NAME = "YOUR_AWS_S3_BUCKET_NAME"
 AWS_ACCESS_KEY = "YOUR_AWS_ACCESS_KEY"
 AWS_SECRET_ACCESS_KEY = "YOUR_AWS_SECRET_ACCESS_KEY"
-IMAGE_STR_TYPES = ('.jpg', '.png')
+IMAGE_STR_TYPES = ('.jpg', '.png', '.jpeg')
+
+AWS_REGION = ""
+AWS_S3_BUCKET_NAME = ""
+AWS_ACCESS_KEY = ""
+AWS_SECRET_ACCESS_KEY = ""
+MONGO_DB_URL=""
 
 if __name__ == '__main__':
     aws_s3_credentials = AWSCredentials_S3(aws_s3_bucket_name=AWS_S3_BUCKET_NAME,
@@ -13,11 +19,11 @@ if __name__ == '__main__':
                                            aws_access_key=AWS_ACCESS_KEY,
                                            aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-    image_prefix = 'some/image/prefix/'
+    image_prefix = "63b5bc8cc5f1cdcad7ef54e7/2/" #'some/image/prefix/'
     image_batch = ImagePathBatch(s3_credentials=aws_s3_credentials)
 
     # read the images
-    image_batch.read(image_prefixes=(image_prefix,),
+    image_batch.read(prefixes=(image_prefix,),
                      valid_image_extensions=IMAGE_STR_TYPES,
                      delimiter='/')
 
