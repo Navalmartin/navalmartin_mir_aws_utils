465620e4b85a7744c00d760e4ef0614241bfb9cf
pockerman <a.giavaras@gmail.com>
2023-02-27 08:29:30 +0000

    Fix delete object from S3

--- src/navalmartin_mir_aws_utils/examples/managing_objects_on_s3.py
+++ src/navalmartin_mir_aws_utils/examples/managing_objects_on_s3.py
@@ -1,5 +1,5 @@
 from navalmartin_mir_aws_utils.aws_credentials import AWSCredentials_S3
-from navalmartin_mir_aws_utils.s3_utils import delete_all_objs_with_key
+from navalmartin_mir_aws_utils.s3_utils import delete_s3_all_objs_with_key, delete_s3_object_with_key
 
 AWS_REGION = "YOUR_AWS_REGION"
 AWS_S3_BUCKET_NAME = "YOUR_AWS_S3_BUCKET_NAME"
@@ -7,13 +7,22 @@ AWS_ACCESS_KEY = "YOUR_AWS_ACCESS_KEY"
 AWS_SECRET_ACCESS_KEY = "YOUR_AWS_SECRET_ACCESS_KEY"
 
 if __name__ == '__main__':
+    # credentials so that we can access S3
     aws_s3_credentials = AWSCredentials_S3(aws_s3_bucket_name=AWS_S3_BUCKET_NAME,
                                            aws_region=AWS_REGION,
                                            aws_access_key=AWS_ACCESS_KEY,
                                            aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-    key = "63b5bc8cc5f1cdcad7ef54e7/"
+    print("---------Deleting one object")
 
-    delete_response = delete_all_objs_with_key(key=key,
-                                               aws_creds=aws_s3_credentials)
+    # delete an object from S3
+    key = "63b5bc8cc5f1cdcad7ef54e7/2/2/img_1.jpeg"
+    delete_response = delete_s3_object_with_key(key=key, s3_client=None, aws_creds=aws_s3_credentials)
+
+    print(delete_response)
+    print("---------Deleting multiple objects")
+    keys = [{'Key': '63b5bc8cc5f1cdcad7ef54e7/2/2/img_2.jpeg'},
+            {'Key': '63b5bc8cc5f1cdcad7ef54e7/2/2/img_3.jpeg'}]
+    delete_response = delete_s3_all_objs_with_key(keys=keys, s3_client=None,
+                                                  aws_creds=aws_s3_credentials)
     print(delete_response)
--- src/navalmartin_mir_aws_utils/s3_utils.py
+++ src/navalmartin_mir_aws_utils/s3_utils.py
@@ -3,6 +3,14 @@ from navalmartin_mir_aws_utils.aws_credentials import AWSCredentials_S3
 from navalmartin_mir_aws_utils.boto3_client import get_aws_s3_client
 
 
+def get_s3_iterator(s3_client: Any, prefix: str,
+                    aws_creds: AWSCredentials_S3, delimiter="/") -> Any:
+    paginator = s3_client.get_paginator('list_objects')
+    return paginator.paginate(Bucket=aws_creds.aws_s3_bucket_name,
+                              Delimiter=delimiter,
+                              Prefix=prefix)
+
+
 def expand_s3_iterator_contents(s3_iterator: Any) -> List[dict]:
     """Get the contents of the provided S3 iterator
 
@@ -34,16 +42,20 @@ def expand_s3_iterator_common_prefixes(s3_iterator: Any) -> List[dict]:
     return [item for item in contents if item is not None]
 
 
-def delete_all_objs_with_key(key: str, s3_client: Any, aws_creds: AWSCredentials_S3,
-                             **options) -> dict:
+def delete_s3_all_objs_with_key(keys: List[dict],
+                                s3_client: Any,
+                                aws_creds: AWSCredentials_S3,
+                                **options) -> dict:
     """Delete all the objects on S3 with the given key.
 
     Parameters
     ----------
-    key: The prefix of the objects
+    keys: The prefix of the objects
     s3_client: The S3 client the objects sit on
     aws_creds: The credentials to use to create an
-    S3 client if the user provided is None
+    S3 client: if the user provided if None an s3 client is built
+    options: Options to pass in the boto3 API call. Currently, the following
+    options are used quiet: True | False
 
     Returns
     -------
@@ -51,6 +63,28 @@ def delete_all_objs_with_key(key: str, s3_client: Any, aws_creds: AWSCredentials
     A dictionary with the response of the operation
     """
 
+    if keys is None or len(keys) == 0:
+        raise ValueError("Invalid key given")
+
+    if aws_creds is None:
+        raise ValueError("AWS credentials not given")
+
+    if s3_client is None:
+        s3_client = get_aws_s3_client(credentials=aws_creds)
+
+    delete_dict = {
+        'Objects': keys,
+        'Quiet': True if 'quiet' in options else False
+    }
+    response = s3_client.delete_objects(Bucket=aws_creds.aws_s3_bucket_name,
+                                        Delete=delete_dict)
+
+    return response
+
+
+def delete_s3_object_with_key(key: str, s3_client: Any,
+                              aws_creds: AWSCredentials_S3,
+                              **options) -> dict:
     if key is None or key == "":
         raise ValueError("Invalid key given")
 
@@ -60,11 +94,12 @@ def delete_all_objs_with_key(key: str, s3_client: Any, aws_creds: AWSCredentials
     if s3_client is None:
         s3_client = get_aws_s3_client(credentials=aws_creds)
 
-    objects = {'Key': key, 'VersionId': options['version_id'] } if 'version_id' in options else {'Key': key, }
-    delete_dict = {
-        'Objects': [objects],
-        'Quiet': True if 'quiet' in options else False
-    }
-    response = s3_client.delete_objects(Bucket=aws_creds.aws_s3_bucket_name,
-                                        Delete=delete_dict)
+    if 'version_id' in options:
+        version_id = options['version_id']
+        response = s3_client.delete_object(Bucket=aws_creds.aws_s3_bucket_name,
+                                           Key=key, VersionId=version_id)
+    else:
+        response = s3_client.delete_object(Bucket=aws_creds.aws_s3_bucket_name,
+                                           Key=key)
+
     return response
