48b516114de2f973965ab7e9655bf8203d1325d8
pockerman <a.giavaras@gmail.com>
2023-03-06 08:28:36 +0000

    Add default parameters in credentials

--- src/navalmartin_mir_aws_utils/aws_credentials.py
+++ src/navalmartin_mir_aws_utils/aws_credentials.py
@@ -16,7 +16,8 @@ class AWSCredentials(object):
 
 class AWSCredentials_S3(AWSCredentials):
     def __init__(self, aws_s3_bucket_name: str, aws_region: str,
-                 aws_access_key: str, aws_secret_access_key: str):
+                 aws_access_key: str = None, 
+                 aws_secret_access_key: str = None):
         super(AWSCredentials_S3, self).__init__(aws_client_name='s3',
                                                 aws_secret_access_key=aws_secret_access_key,
                                                 aws_access_key=aws_access_key)
@@ -26,8 +27,9 @@ class AWSCredentials_S3(AWSCredentials):
 
 class AWSCredentials_SQS(AWSCredentials):
     def __init__(self, aws_sqs_queue_name: str, aws_region: str,
-                 aws_access_key: str, aws_secret_access_key: str,
-                 aws_queue_url: str):
+                 aws_queue_url: str,
+                 aws_access_key: str = None, 
+                 aws_secret_access_key: str = None):
         super(AWSCredentials_SQS, self).__init__(aws_client_name='sqs',
                                                  aws_secret_access_key=aws_secret_access_key,
                                                  aws_access_key=aws_access_key)
@@ -41,8 +43,8 @@ class AWSCredentials_CognitoIDP(AWSCredentials):
                  aws_cognito_pool_id: str,
                  aws_cognito_client_id: str,
                  aws_cognito_client_secret: str,
-                 aws_access_key: str,
-                 aws_secret_access_key: str):
+                 aws_access_key: str = None,
+                 aws_secret_access_key: str = None):
         super(AWSCredentials_CognitoIDP, self).__init__(aws_client_name="cognito-idp",
                                                         aws_access_key=aws_access_key,
                                                         aws_secret_access_key=aws_secret_access_key)
