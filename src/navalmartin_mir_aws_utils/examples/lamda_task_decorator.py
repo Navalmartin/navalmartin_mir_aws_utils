7b00601d59d75a4e52b3424a15b48442cb557705
pockerman <a.giavaras@gmail.com>
2023-03-08 09:45:01 +0000

    Add lambda task decorator

---
+++ src/navalmartin_mir_aws_utils/examples/lamda_task_decorator.py
@@ -0,0 +1,27 @@
+import asyncio
+from navalmartin_mir_aws_utils.tasks_lambdas import aws_lambda_task_decorator
+
+
+def before_task_runner(args: dict):
+    print(f"Running task with arguments {args}")
+    return "Finished before task runner"
+
+
+def after_task_runner(args: dict):
+    print(f"Running task with arguments {args}")
+    return "Finished after task runner"
+
+
+@aws_lambda_task_decorator(before_task_runner=before_task_runner,
+                           after_task_runner=after_task_runner,
+                           after_task_runner_args={},
+                           before_task_runner_args={})
+def task(args: dict):
+    print(f"Running task with arguments {args}")
+    return "None"
+
+
+if __name__ == '__main__':
+
+    result = task({})
+    print(result)
