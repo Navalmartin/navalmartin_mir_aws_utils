d15b81d3ad7e980692dca4b39553d440851747dc
pockerman <a.giavaras@gmail.com>
2023-03-10 08:52:50 +0000

    Add conditional execution for decorator

--- src/navalmartin_mir_aws_utils/examples/lamda_task_decorator.py
+++ src/navalmartin_mir_aws_utils/examples/lamda_task_decorator.py
@@ -3,19 +3,17 @@ from navalmartin_mir_aws_utils.tasks_lambdas import aws_lambda_task_decorator
 
 
 def before_task_runner(args: dict):
-    print(f"Running task with arguments {args}")
+    print(f"Running before_task_runner with arguments {args}")
     return "Finished before task runner"
 
 
 def after_task_runner(args: dict):
-    print(f"Running task with arguments {args}")
+    print(f"Running after_task_runner with arguments {args}")
     return "Finished after task runner"
 
 
 @aws_lambda_task_decorator(before_task_runner=before_task_runner,
-                           after_task_runner=after_task_runner,
-                           after_task_runner_args={},
-                           before_task_runner_args={})
+                           after_task_runner=after_task_runner)
 def task(args: dict):
     print(f"Running task with arguments {args}")
     return "None"
--- src/navalmartin_mir_aws_utils/tasks_lambdas/aws_tasks_decorator.py
+++ src/navalmartin_mir_aws_utils/tasks_lambdas/aws_tasks_decorator.py
@@ -3,16 +3,18 @@ from typing import Callable
 
 
 def async_aws_lambda_task_decorator(async_before_task_runner: Callable,
-                                    async_after_task_runner: Callable,
-                                    before_task_runner_args: dict,
-                                    after_task_runner_args: dict):
+                                    async_after_task_runner: Callable):
     def _execute_lambda_task(async_lambda_task_fn: Callable):
         @wraps(async_lambda_task_fn)
         async def wrapper(*args, **kwargs):
-            before_task_runner_result = await async_before_task_runner(before_task_runner_args)
+            if async_before_task_runner is not None:
+                before_task_runner_result = await async_before_task_runner(*args, **kwargs)
+
             lambda_task_result = await async_lambda_task_fn(*args, **kwargs)
-            after_task_runner_result = await async_after_task_runner(after_task_runner_args)
-            return before_task_runner_result, lambda_task_result, after_task_runner_result
+
+            if async_after_task_runner is not None:
+                after_task_runner_result = await async_after_task_runner(*args, **kwargs)
+            return lambda_task_result
 
         return wrapper
 
@@ -20,16 +22,17 @@ def async_aws_lambda_task_decorator(async_before_task_runner: Callable,
 
 
 def aws_lambda_task_decorator(before_task_runner: Callable,
-                              after_task_runner: Callable,
-                              before_task_runner_args: dict,
-                              after_task_runner_args: dict):
+                              after_task_runner: Callable):
     def _execute_lambda_task(lambda_task_fn: Callable):
         @wraps(lambda_task_fn)
         def wrapper(*args, **kwargs):
-            before_task_runner_result = before_task_runner(before_task_runner_args)
+            if before_task_runner is not None:
+                before_task_runner_result = before_task_runner(*args, **kwargs)
             lambda_task_result = lambda_task_fn(*args, **kwargs)
-            after_task_runner_result = after_task_runner(after_task_runner_args)
-            return before_task_runner_result, lambda_task_result, after_task_runner_result
+
+            if after_task_runner is not None:
+                after_task_runner_result = after_task_runner(*args, **kwargs)
+            return lambda_task_result
 
         return wrapper
 
