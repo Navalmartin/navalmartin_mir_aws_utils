85e011b8717053740f69f2bcc47563b1093c69f8
pockerman <a.giavaras@gmail.com>
2023-03-19 17:22:42 +0000

    Remove AWS_KEY and AWS_SECRET_KEY

--- src/navalmartin_mir_aws_utils/examples/working_with_sqs.py
+++ src/navalmartin_mir_aws_utils/examples/working_with_sqs.py
@@ -4,8 +4,6 @@ from navalmartin_mir_aws_utils.sqs_utils import send_sqs_message, read_one_sqs_m
 
 AWS_REGION = "YOUR_AWS_REGION"
 AWS_S3_BUCKET_NAME = "YOUR_AWS_S3_BUCKET_NAME"
-AWS_ACCESS_KEY = "YOUR_AWS_ACCESS_KEY"
-AWS_SECRET_ACCESS_KEY = "YOUR_AWS_SECRET_ACCESS_KEY"
 AWS_SQS_URL = "YOUR_AWS_SQS_URL"
 AWS_SQS_NAME = "YOUR_AWS_SQS_NAME"
 AWS_SQS_GROUP_ID = "YOUR_AWS_SQS_GROUP_ID"
@@ -14,9 +12,7 @@ AWS_SQS_GROUP_ID = "YOUR_AWS_SQS_GROUP_ID"
 if __name__ == '__main__':
     aws_sqs_credentials = AWSCredentials_SQS(aws_sqs_queue_name=AWS_SQS_NAME,
                                              aws_queue_url=AWS_SQS_URL,
-                                             aws_region=AWS_REGION,
-                                             aws_access_key=AWS_ACCESS_KEY,
-                                             aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+                                             aws_region=AWS_REGION)
 
     message = "task_id:123"
     sqs_msg = SQSMessageConfig(message_body=message,
