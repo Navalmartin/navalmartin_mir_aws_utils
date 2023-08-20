e1d27238803f0e999d641752b68445e33acaf5ae
pockerman <a.giavaras@gmail.com>
2023-08-20 07:35:10 +0100

    Remove not used functionality

--- src/navalmartin_mir_aws_utils/__init__.py
+++ src/navalmartin_mir_aws_utils/__init__.py
@@ -3,7 +3,7 @@ Various utilities for working with AWS in Python. The official
 PyPi package can be found <a href="https://pypi.org/project/navalmartin-mir-aws-utils/">here</a>.
 This package is basically wrappers on top of boto3 that allows for easier development with AWS.
 """
-__version__ = "0.0.35"
+__version__ = "0.0.36"
 from .boto3_client import get_aws_client_factory
 from .aws_credentials import (
     AWSCredentials_S3,
@@ -27,7 +27,14 @@ from .s3_utils import (get_s3_iterator,
 from .utils.simple_email import (Email,
                                  EmailBody,
                                  EmailSubject)
-
+from .tasks_lambdas.aws_tasks_decorator import (aws_lambda_task_decorator,
+                                                async_aws_lambda_task_decorator)
 from .tasks.ses_tasks.ses_tasks import (verify_email_identity,
                                         send_simple_email,
                                         send_html_email)
+
+from .sqs_utils import send_sqs_message, read_one_sqs_message, delete_sqs_message
+from .sqs_queue_config import MessageAttributes, SQSMessageConfig
+
+
+
--- src/navalmartin_mir_aws_utils/tasks_lambdas/__init__.py
+++ src/navalmartin_mir_aws_utils/tasks_lambdas/__init__.py
@@ -1,5 +1,3 @@
-from .tasks_lambdas_utils import get_error_return, get_success_return, get_secrets
-from .tasks_lambdas_utils import validate_sqs_event_record
 from .aws_tasks_decorator import (
     aws_lambda_task_decorator,
     async_aws_lambda_task_decorator,
